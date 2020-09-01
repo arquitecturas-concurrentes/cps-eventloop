045d86baa2c62e18ef83acbd7218e9f04ede8bbf
Ernesto Bossi <bossi.ernestog@gmail.com>
2020-09-01 12:08:38 -0300

    Create 003_call_stack.js

---
+++ 003_call_stack.js
@@ -0,0 +1,9 @@
+// http://latentflip.com/loupe/?code=ZnVuY3Rpb24gbXVsdGlwbHkoeCwgeSkgewogICAgcmV0dXJuIHggKiB5Owp9CmZ1bmN0aW9uIHByaW50U3F1YXJlKHgpIHsKICAgIHZhciBzID0gbXVsdGlwbHkoeCwgeCk7CiAgICBjb25zb2xlLmxvZyhzKTsKfQpwcmludFNxdWFyZSg1KQ%3D%3D!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
+function multiply(x, y) {
+    return x * y;
+}
+function printSquare(x) {
+    var s = multiply(x, x);
+    console.log(s);
+}
+printSquare(5)
